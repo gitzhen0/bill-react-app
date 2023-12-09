bfc9a508451d5ca26dbcf091ae07e493eb400023
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-09 15:48:13 -0500

    p64 : day4-13 : antD theme color setup

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd-mobile'
 
 const Layout = () => {
   return (
     <div>
       <Outlet />
       i am layout
+      <Button color="primary">test all everywhere</Button>
+      <div className='purple'>
+        <Button color="primary">test local</Button>
+      </div>
     </div>
   )
 }

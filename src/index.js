5d5578799de55aa3aa32e41009f95edfdb3cc708
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2023-12-09 14:12:29 -0500

    day4-10 配置别名路径@

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import sum from '@/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const total = sum(1, 3)
+console.log(total)
+
 root.render(
   <App />
 );
